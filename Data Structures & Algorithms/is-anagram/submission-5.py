921274355e27af606dde81464dc24b1d8abb4279
zaka <zb2003boujana@gmail.com>
2026-05-06 13:29:59 +0200

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        return sorted(s) == sorted(t)
+
+        # freq = Counter(s)
+        
+        # for char in t:
+        #     if char in freq:
+        #         if freq[char] != 1:
+        #             freq[char] -= 1
+        #         else:
+        #             del freq[char]
+        #     else:
+        #         return False
+
+        # return True
